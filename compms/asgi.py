70143740b4c7cc769f1e21372d6ad1c7cc32b672
Sumit Thakare <sumitthakare1122@gmail.com>
2026-03-03 16:28:46 +0530

    update missing files

--- compms/asgi.py
+++ compms/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temp_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compms.settings')
 
 application = get_asgi_application()
--- compms/wsgi.py
+++ compms/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'temp_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compms.settings')
 
 application = get_wsgi_application()
